3400557c6ac41e236917a6b86da08cf13446106e
fei <fei@wujianxunde-MacBook-Pro.local>
2023-03-22 16:50:12 +0800

    first commit

---
+++ .terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.51.0"
+  constraints = "4.51.0"
+  hashes = [
+    "h1:iax8rUF2cBEERGs8vsEAiNnxrZqeWpMHoWVYTf/P2po=",
+    "zh:001bf7478e495d497ffd4054453c97ab4dd3e6a24d46496d51d4c8094e95b2b1",
+    "zh:19db72113552dd295854a99840e85678d421312708e8329a35787fff1baeed8b",
+    "zh:42c3e629ace225a2cb6cf87b8fabeaf1c56ac8eca6a77b9e3fc489f3cc0a9db5",
+    "zh:50b930755c4b1f8a01c430d8f688ea79de0b0198c87511baa3a783e360d7e624",
+    "zh:5acd67f0aafff5ad59e179543cccd1ffd48d69b98af0228506403b8d8193b340",
+    "zh:70128d57b4b4bf07df941172e6af15c4eda8396af5cc2b0128c906983c7b7fad",
+    "zh:7905fac0ba2becf0e97edfcd4224e57466b04f960f36a3ec654a0a3c2ffececb",
+    "zh:79b4cc760305cd77c1ff841f789184f808b8052e8f4faa5cb8d518e4c13beb22",
+    "zh:c7aebd7d7dd2b29de28e382500d36fae8b4d8a192cf05e41ea29c66f1251acfc",
+    "zh:d8b4494b13ef5af65d3afedf05bf7565918f1e31ad68ae0df81f5c3b12baf519",
+    "zh:e6e68ef6881bc3312db50c9fd761f226f34d7834b64f90d96616b7ca6b1daf34",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version = "4.58.0"
+  hashes = [
+    "h1:0ytJMBnEoX67hFw1xhIODZL6QEzqICcWNbc/OyzU9VY=",
+    "zh:1e94926ec4b5329b7d667b9b0ecae6a7fb5602ff01b22f77f5bceb1feb90d85e",
+    "zh:4f119d43ae1dde96db405b8b8d228631c49a5eaf1e8b57024d8840878b6dbf95",
+    "zh:60eb2b9fbe12d86dad897076f3ab4838eebfd7aa30546b0cf9d73e04e5401f00",
+    "zh:61c94e5590fe5a8739fc78dcbcb5de693aa7b599deda7d404510747cff67bb08",
+    "zh:87d857e16432931c73d0897de07b79fe45698c5923cb4f2eaeaa6cb33f9819e0",
+    "zh:c82f18b41dbe17482741fa0ca5e22445bd854d17cda0471b821175ec3c20e2b8",
+    "zh:e191ed4ae753564187770c8ae20455728830ec8b097488a169000f2e89b44473",
+    "zh:e5923d6800154b809148f82b1b983003eed751e3678d7a18dd870eda571d1de1",
+    "zh:ece45f7a576111b4bac866fe1f5c590f8ffde5a295cb4f848d11c0f994de06cc",
+    "zh:f10e604bb659c280f5e00da7876719c3838b6aa9322450ea905788b43f5e77d9",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:feb303fb5163b27138cf148b7f1a4bb0d91351bb40e571930607f55987a11095",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcp-tf-sa.json")
+
+  project = "datacloud-lab"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+provider "google-beta" {
+  credentials = file("gcp-tf-sa.json")
+
+  project = "datacloud-lab"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "datacloud_lab_vpc_network_1" {
+  name                    = "datacloud-lab-vpc-1"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "network_with_private_subnet_1" {
+  name                     = "datacloud-lab-subnet-us-central1"
+  ip_cidr_range            = "10.128.0.0/24"
+  region                   = "us-central1"
+  network                  = google_compute_network.datacloud_lab_vpc_network_1.id
+  private_ip_google_access = true
+}
+
+resource "google_compute_subnetwork" "network_with_private_subnet_2" {
+  name                     = "datacloud-lab-subnet-asia-east1"
+  ip_cidr_range            = "10.128.1.0/24"
+  region                   = "asia-east1"
+  network                  = google_compute_network.datacloud_lab_vpc_network_1.id
+  private_ip_google_access = true
+}
+
+resource "google_compute_firewall" "tcp_80_allow_rule" {
+  name    = "datacloud-lab-internet-subnet-tcp-80-allow-rule"
+  network = google_compute_network.datacloud_lab_vpc_network_1.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["10.128.0.0/24", "10.128.1.0/24"]
+  direction     = "INGRESS"
+  priority      = 1000
+}
+
+resource "google_compute_firewall" "tcp_443_allow_rule" {
+  name    = "datacloud-lab-internet-subnet-tcp-443-allow-rule"
+  network = google_compute_network.datacloud_lab_vpc_network_1.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["10.128.0.0/24", "10.128.1.0/24"]
+  direction     = "INGRESS"
+  priority      = 1000
+}
+
+resource "google_compute_firewall" "tcp_22_allow_rule" {
+  name    = "datacloud-lab-internet-subnet-tcp-22-allow-rule"
+  network = google_compute_network.datacloud_lab_vpc_network_1.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["10.128.0.0/24", "10.128.1.0/24"]
+  direction     = "INGRESS"
+  priority      = 65534
+}
+
+resource "google_compute_firewall" "icmp_allow_rule" {
+  name    = "datacloud-lab-internet-subnet-icmp-allow-rule"
+  network = google_compute_network.datacloud_lab_vpc_network_1.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.128.0.0/24", "10.128.1.0/24"]
+  direction     = "INGRESS"
+  priority      = 65534
+}
